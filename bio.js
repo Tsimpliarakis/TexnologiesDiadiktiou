af07f74c7ea62e51156dea4d175531796cdaa7b8
tsimpliarakis <m.tsimpliarakis@gmail.com>
2022-04-05 21:51:34 +0300

    update

--- bio.js
+++ bio.js
@@ -3,6 +3,8 @@ function toggleTheme() {
   // elements.
   // Select your element using indexing.
   var theme1 = document.getElementsByTagName('link')[1];
+  var theme2 = document.getElementsByTagName('link')[2];
+  var theme3 = document.getElementsByTagName('link')[3];
 
   // Change the value of href attribute 
   // to change the css sheet.
@@ -12,20 +14,34 @@ function toggleTheme() {
       theme1.setAttribute('href', 'header.css');
   }
 
-  var theme2 = document.getElementsByTagName('link')[2];
-
+  if (theme3.getAttribute('href') == 'footer.css') {
+      theme3.setAttribute('href', 'footer-acc.css');
+  } else {
+      theme3.setAttribute('href', 'footer.css');
+  }
+  // Index
   if (theme2.getAttribute('href') == 'index.css') {
       theme2.setAttribute('href', 'index-acc.css');
-  } else {
+  } else if (theme2.getAttribute('href') == 'index-acc.css') {
       theme2.setAttribute('href', 'index.css');
   }
-
-  var theme2 = document.getElementsByTagName('link')[3];
-
-  if (theme2.getAttribute('href') == 'footer.css') {
-      theme2.setAttribute('href', 'footer-acc.css');
-  } else {
-      theme2.setAttribute('href', 'footer.css');
+  // Catalogue
+  if (theme2.getAttribute('href') == 'catalogue.css') {
+    theme2.setAttribute('href', 'catalogue-acc.css');
+  } else if (theme2.getAttribute('href') == 'catalogue-acc.css') {
+    theme2.setAttribute('href', 'catalogue.css');
+  }
+  // Info
+  if (theme2.getAttribute('href') == 'info.css') {
+      theme2.setAttribute('href', 'info-acc.css');
+  } else if (theme2.getAttribute('href') == 'info-acc.css') {
+      theme2.setAttribute('href', 'info.css');
+  }
+  // Form
+  if (theme2.getAttribute('href') == 'form.css') {
+      theme2.setAttribute('href', 'form-acc.css');
+  } else if (theme2.getAttribute('href') == 'form-acc.css') {
+      theme2.setAttribute('href', 'form.css');
   }
 }
 
